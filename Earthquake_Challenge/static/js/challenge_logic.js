f17bb393f75c25e5d264fc68abc2f91989657f68
Choua Vang <cvangx286@gmail.com>
2022-07-05 18:57:36 -0500

    updating challenge

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -146,20 +146,7 @@ legend.onAdd = function() {
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
   d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(tectonicPlateData) {
     console.log(tectonicPlateData);
-    L.geoJSON(tectonicPlateData, {
-      // We turn each feature into a circleMarker on the map.
-      pointToLayer: function(feature, latlng) {
-            console.log(data);
-            return L.circleMarker(latlng);
-            },
-              // We set the style for each circleMarker using our styleInfo function.
-      style: styleInfo,
-      // We create a popup for each circleMarker to display the magnitude and
-      //  location of the earthquake after the marker has been created and styled.
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-      }
-    }).addTo(earthquakes);
+    L.geoJSON(tectonicPlateData).addTo(tectonicPlateData);
     tectonicPlateData.addTo(map);
   });
 });
